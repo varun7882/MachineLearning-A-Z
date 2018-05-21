1736536f97733b83268fd33a51fa6eefd2c93b87
varun7882 <varunparas.srivastava8@gmail.com>
2018-05-21 23:05:45 +0530

    reverting redundant folder

--- ComparisonsinClassifications/LogisticRegression.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-dataset=pd.read_csv("iris.data.csv")
-X=dataset.iloc[:,0:4].values
-y=dataset.iloc[:,4].values
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 0)
